7721d8821981179d722013aa6f34bee99d6313e4
patriciofrank <patricio-agustin@hotmail.com>
2023-02-08 20:39:27 -0300

    cartCreation

--- src/controllers/CartManager.js
+++ src/controllers/CartManager.js
@@ -3,14 +3,19 @@ import { ProductManager } from "../controllers/ProductManager.js";
 const productos =await JSON.parse(await fs.readFile(ProductManager("./models/products.json"), 'utf-8'))
 
 export class CartManager{ 
+    constructor(path) {
+        this.path = path;
+    }
 
-    inTheCart = (id) => {
+  async  inTheCart (id) {
     // verify if the id is in cart
-    const productoExist = productos.some((elemento) => elemento.id === id);
+    const cartP = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+    const productoExist = cartP.some((elemento) => elemento.id === id);
     return productoExist;
   };
 
- addProduct = (item, cantidad) => {
+ async addProduct (item, cantidad)  {
+    const cartP = JSON.parse(await fs.readFile(this.path, 'utf-8'))
     const productosB = [...productos];
     //modificate product cart quantity
     if (inTheCart(item.id)) {
@@ -18,28 +23,37 @@ export class CartManager{
         (elemento) => elemento.id === item.id
       );
       productosB[productRep].cantidad += cantidad;
-      productosB[productRep].precioCantidad =
-        productosB[productRep].cantidad * productosB[productRep].price;
-      setProductos(productosB);
     } else {
       // add newproduct to cart
       const newProduct = {
         ...item,
         cantidad: cantidad,
-        precioCantidad: cantidad * item.price,
       };
       productosB.push(newProduct);
-      setProductos(productosB);
+      await fs.writeFile(this.path, JSON.stringify(cartP, null, 2));
+      return "product add succesfuly"
     }
   };
 
 
-   deleteCart = () => {
-    const cart=[];
-
+   async deleteCart () {
+    const cartP = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+    cartP=[];
+    await fs.writeFile(this.path, JSON.stringify(cartP, null, 2));
+    return "cart deleted"
   };
-   deleteItem = (id) => {
-    const newProduct = productos.filter((element) => element.id !== id);
-    setProductos(newProduct);
+  async deleteItem (id)  {
+    try {
+        const cartP = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+        if (cartP.some(prod => prod.id === parseInt(id))) {
+            const cartFilter = prods.filter(prod => prod.id !== parseInt(id))
+            await fs.writeFile(this.path, JSON.stringify(cartFilter,null,2))
+            return "Producto eliminado"
+        } else {
+            return "Producto no encontrado"
+        }
+    } catch (error) {
+        return error
+    }
   };
 }
