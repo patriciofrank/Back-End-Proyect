a39dced58647ede2c640b28090bad5cf6bd75fe7
patriciofrank <patricio-agustin@hotmail.com>
2023-02-15 22:02:54 -0300

    websokets

--- src/app.js
+++ src/app.js
@@ -1,45 +1,59 @@
 //create server
-import express from"express";
-import routerProd  from './routes/product.js'
+import express from "express";
+import routerProd from './routes/product.js'
 import routerCart from "./routes/cart.js";
-import {__dirname} from './utils/path.js'
+import { __dirname } from './path.js'
 import { engine } from "express-handlebars";
 import * as path from "path"
+import { Server } from "socket.io";
 
 const PORT = 4000;
 const app = express();
+const server = app.listen(PORT, () => {
+  const msg = `
+      Server listen on http://localhost:${PORT}
+        
+      Pruebas:
+    
+      http://localhost:${PORT}/api/products
+      http://localhost:${PORT}/api/products?limit=5
+      http://localhost:${PORT}/api/product/1
+      http://localhost:${PORT}/api/cart
+      http://localhost:${PORT}/api/cart/1
+    `;
+  console.log(msg);
+});
+
+const io = new Server(server)
 //middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.engine("handlebars",engine())
-app.set("view engine","handlebars")
-app.set("views", path.resolve(__dirname,"./views"))
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", path.resolve(__dirname, "./views"))
 
 //routes
-app.use('/static', express.static(__dirname + '/../public'))
+app.use('/static', express.static(__dirname + './public'))
 app.use('/api/products', routerProd)
 app.use('/api/cart', routerCart)
 
-app.get('/static',(req,res)=>{
-  res.render("home",{
-    title:"BackendProyect",
-    mensage:"to my backend proyect"
+app.get('/', (req, res) => {
+  console.log( routerProd.get('/'))
+  // const products=routerProd.get('/')
+  res.render("home", {
+    title: "BackendProyect",
+    // products
   })
 })
 
+io.on("connection", (socket) => {
+  console.log("Connected to socket")
+
+  socket.on('message', info => { console.log(info) })
+
+  socket.broadcast.emit('event-admin', 'hi you are the admin') 
+
+  socket.emit('event-general', "Hi to every users")
+})
 
 
-app.listen(PORT, () => {
-    const msg = `
-        Server listen on http://localhost:${PORT}
-          
-        Pruebas:
-      
-        http://localhost:${PORT}/api/products
-        http://localhost:${PORT}/api/products?limit=5
-        http://localhost:${PORT}/api/product/1
-        http://localhost:${PORT}/api/cart
-        http://localhost:${PORT}/api/cart/1
-      `;
-    console.log(msg);
-});
