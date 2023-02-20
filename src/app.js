52f6f86e95fa0b765499dda2b84f6505f36ce12a
patriciofrank <patricio-agustin@hotmail.com>
2023-02-20 19:01:52 -0300

    realtimeProducts

--- src/app.js
+++ src/app.js
@@ -7,71 +7,56 @@ import { engine } from "express-handlebars";
 import * as path from "path"
 import { Server } from "socket.io";
 import { ProductManager } from "./controllers/ProductManager.js";
-import * as http from 'http'
 const products = new ProductManager("src/models/products.json");
-const app = express();
 
-// //middlewares
+const PORT = 8080;
+const app = express();
+const server = app.listen(PORT, () => {
+  const msg = `
+      Server listen on http://localhost:${PORT}
+        
+      Pruebas:
+    
+      http://localhost:${PORT}/api/products
+      http://localhost:${PORT}/api/products?limit=5
+      http://localhost:${PORT}/api/product/1
+      http://localhost:${PORT}/api/cart
+      http://localhost:${PORT}/api/cart/1
+    `;
+  console.log(msg);
+});
+const io = new Server(server)
+//middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.engine("handlebars", engine())
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars")
+app.set("views", path.resolve(__dirname, "./views"))
 
 //routes
-app.use('/', express.static(__dirname + './public'))
+app.use('/static', express.static(__dirname + './public'))
 app.use('/api/products', routerProd)
 app.use('/api/cart', routerCart)
 
-
-const server = http.createServer(app);
-
-
-const io = new Server(server);
-
-
-app.get("/", async (req, res) => {
-  const productsList = products.getProducts();
-
+app.get('/',async (req, res) => {
+  const productsList = await products.getProducts()
   res.render("home", {
     title: "BackendProyect",
     productsList
   })
-
-});
-
-
-io.on("connection", socket => {
-
-  console.log("New conection", socket.id);
-
-  socket.on("add-product", product => {
-    products.addProduct(product);
-    io.emit("update-products", product);
-  });
-
-  socket.on("disconnect", () => {
-    console.log(socket.id, "disconnected");
+})
+app.get('/realtimeProducts',async (req, res) => {
+  const productsList = await products.getProducts()
+  res.render("home", {
+    title: "BackendProyect",
+    productsList
   })
-});
+})
 
 
-app.use((err, req, res, next) => {
-
-  console.log(err);
-
-  res.status(500).json({ err, message: "Something went wrong, sorry" });
-});
-
-const PORT = process.env.PORT || 8080;
-
-server.listen(PORT, () => {
-
-  console.log(`Servidor corriendo en puerto http://localhost:${PORT}`);
-
-});
-
-server.on("error", err => {
-  console.log(`Algo salio mal: ${err}`);
-
+io.on("connection", (socket) => {
+  console.log("Connected to socket")
+  socket.on('message', info => { console.log(info) })
+  socket.broadcast.emit('event-admin', 'hi you are the admin') 
+  socket.emit('event-general', "Hi to every users")
 })
