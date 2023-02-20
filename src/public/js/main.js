b176804d0268bfbeff4740fcf67989b9ae41673b
patriciofrank <patricio-agustin@hotmail.com>
2023-02-20 10:03:41 -0300

    hbs

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,12 +1,32 @@
-const socket=io()
+const socket = io();
 
+const form = document.querySelector("#form");
+const titleInput = document.querySelector("#title");
+const priceInput = document.querySelector("#price");
+const thumbnailInput = document.querySelector("#thumbnail");
+const table = document.querySelector("#table");
 
-socket.emit('message','Hola me estoy conectando')
+form.addEventListener("submit", e => {
+e.preventDefault();
+if (titleInput.value && priceInput.value && thumbnailInput.value) {
+const product = {
+title: titleInput.value,
+price: priceInput.value,
+thumbnail: thumbnailInput.value,
+};
 
-socket.on('evento-admin', datos => {
-    console.log(datos)
-})
+socket.emit("add-product", product);
+titleInput.value = "";
+priceInput.value = "";
+thumbnailInput.value = "";
+}
+});
 
-socket.on('evento-general', datos => {
-    console.log(datos)
-})
+socket.on("update-products", product => {
+const template = Handlebars.compile(
+"<td>{{title}}</td><td>{{price}}</td><td><img src={{thumbnail}} style='width:60px;'></img></td>"
+);
+const tr = document.createElement("tr");
+tr.innerHTML = template(product);
+table.appendChild(tr);
+});
